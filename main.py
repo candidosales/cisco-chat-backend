806ac89b2a0e7a618e6ef8327a4c21af5b4f2475
Candido Sales Gomes <candidosg@gmail.com>
2023-06-29 22:03:21 -0400

    read file works

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,20 +8,38 @@ from embedchain import App
 
 chat_bot = App()
 
-chat_bot.add("youtube_video", "https://www.youtube.com/watch?v=3qHkcs3kG44")
+
+def etl():
+    with open("./data/output-2023-06-22T03:37:39.362Z.csv", "r") as file:
+        csvreader = csv.reader(file)
+        next(csvreader, None)
+        for row in csvreader:
+            print(row[0])
+            chat_bot.add("web_page", row[0])
 
 
-chat_bot.add_local(
-    "qna_pair",
-    (
-        "Who is Naval Ravikant?",
-        "Naval Ravikant is an Indian-American entrepreneur and investor.",
-    ),
-)
+etl()
 
 
-print(
-    chat_bot.query(
-        "What unique capacity does Naval argue humans possess when it comes to understanding explanations or concepts?"
-    )
-)
+# from embedchain import App
+
+# chat_bot = App()
+
+
+# chat_bot.add("youtube_video", "https://www.youtube.com/watch?v=3qHkcs3kG44")
+
+
+# chat_bot.add_local(
+#     "qna_pair",
+#     (
+#         "Who is Naval Ravikant?",
+#         "Naval Ravikant is an Indian-American entrepreneur and investor.",
+#     ),
+# )
+
+
+# print(
+#     chat_bot.query(
+#         "What unique capacity does Naval argue humans possess when it comes to understanding explanations or concepts?"
+#     )
+# )
