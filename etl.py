57198b7f4c01f29910712b4f35e42191500891f8
Candido Sales Gomes <candidosg@gmail.com>
2023-07-01 13:25:35 -0400

    git add save in chromadb properly

--- etl.py
+++ etl.py
@@ -1,36 +1,96 @@
 import csv
-from time import sleep
 from multiprocessing import Pool
-
 from dotenv import load_dotenv
 
 load_dotenv()
+
+from loaders.web_page import WebPageLoader
+from chunckers.web_page import WebPageChunker
 
 from embedchain import App
 
 chat_bot = App()
 
 
-def add_into_db(url):
-    print("add_into_db", url, flush=True)
-    sleep(1)
-    chat_bot.add("web_page", url)
-
-
-def read_csv():
+def read_csv(max_items: int):
     with open("./data/output-2023-06-22T03:37:39.362Z.csv", "r") as file:
         csvreader = csv.reader(file)
         next(csvreader, None)
         urls = []
-        for row in csvreader:
+        for index, row in enumerate(csvreader):
+            if index + 1 == max_items:
+                break
             urls.append(row[0])
             # chat_bot.add("web_page", row[0])
 
         return urls
 
 
-if __name__ == "__main__":
-    urls = read_csv()
+def add_into_db(url: str):
+    print("add_into_db", url, flush=True)
+    chat_bot.user_asks.append(["web_page", url])
+    load_and_embed(WebPageLoader(), WebPageChunker(), url)
+
+
+def add_into_db_parallel(urls: list[str]):
     with Pool() as pool:
         result = pool.map(add_into_db, urls)
     print("Program finished!", flush=True)
+
+
+def add_into_db_sync(urls: list[str]):
+    for url in urls:
+        chat_bot.user_asks.append(["web_page", url])
+        load_and_embed(WebPageLoader(), WebPageChunker(), url)
+    print("Program finished! - sync")
+
+
+def check_output(url: str):
+    return WebPageLoader().load_data(url)
+
+
+def load_and_embed(loader, chunker, url):
+    """
+    Loads the data from the given URL, chunks it, and adds it to the database.
+
+    :param loader: The loader to use to load the data.
+    :param chunker: The chunker to use to chunk the data.
+    :param url: The URL where the data is located.
+    """
+    embeddings_data = chunker.create_chunks(loader, url)
+    print("[load_and_embed] embeddings_data", embeddings_data)
+    documents = embeddings_data["documents"]
+    metadatas = embeddings_data["metadatas"]
+    ids = embeddings_data["ids"]
+    # get existing ids, and discard doc if any common id exist.
+    existing_docs = chat_bot.collection.get(
+        ids=ids,
+        # where={"url": url}
+    )
+    existing_ids = set(existing_docs["ids"])
+
+    if len(existing_ids):
+        data_dict = {
+            id: (doc, meta) for id, doc, meta in zip(ids, documents, metadatas)
+        }
+        data_dict = {
+            id: value for id, value in data_dict.items() if id not in existing_ids
+        }
+
+        if not data_dict:
+            print(f"All data from {url} already exists in the database.")
+            return
+
+        ids = list(data_dict.keys())
+        documents, metadatas = zip(*data_dict.values())
+
+    chat_bot.collection.add(documents=documents, metadatas=metadatas, ids=ids)
+    print(
+        f"Successfully saved {url}. Total chunks count: {chat_bot.collection.count()}"
+    )
+
+
+if __name__ == "__main__":
+    urls = read_csv(20)
+    if len(urls) > 0:
+        add_into_db_sync(urls)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from models import Conversation
+from langchain.docstore.document import Document
 
 load_dotenv()
 
@@ -14,10 +15,87 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"app": "Cisco Security Advisories"}
 
 
 @app.post("/conversation")
 def conversation(conversation: Conversation):
-    # return {"conversation": conversation.messages[0].content.message}
-    return chat_bot.query("What's CVE-2023-20028")
+    # return {"conversation": conversation.messages[0].con
+    # tent.message}
+    # return chat_bot.query(conversation.messages[0].content.message)
+    return query(conversation.messages[0].content.message)
+
+
+def retrieve_from_database(input_query: str, number_documents: int):
+    """
+    Queries the vector database based on the given input query.
+    Gets relevant doc based on the query
+
+    :param input_query: The query to use.
+    :return: The content of the document that matched your query.
+    """
+
+    result = chat_bot.collection.query(
+        query_texts=[
+            input_query,
+        ],
+        n_results=number_documents,
+    )
+
+    result_formatted = _format_result(result)
+
+    contents = [document[0].page_content for document in result_formatted]
+
+    return contents
+
+
+def _format_result(results):
+    return [
+        (Document(page_content=result[0], metadata=result[1] or {}), result[2])
+        for result in zip(
+            results["documents"][0],
+            results["metadatas"][0],
+            results["distances"][0],
+        )
+    ]
+
+
+def generate_prompt(input_query: str, contexts: list[str]):
+    """
+    Generates a prompt based on the given query and context, ready to be passed to an LLM
+
+    :param input_query: The query to use.
+    :param context: Similar documents to the query used as context.
+    :return: The prompt
+    """
+
+    if type(contexts) is list and len(contexts) > 0:
+        prompt = f"""Use the following pieces of context to answer the query at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+            {" | ".join(contexts)}
+            Query: {input_query}
+            Helpful Answer:
+            """
+
+    else:
+        prompt = f"""Use the following pieces of context to answer the query at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+            {contexts[0]}
+            Query: {input_query}
+            Helpful Answer:
+            """
+    return prompt
+
+
+def query(input_query):
+    """
+    Queries the vector database based on the given input query.
+    Gets relevant doc based on the query and then passes it to an
+    LLM as context to get the answer.
+
+    :param input_query: The query to use.
+    :return: The answer to the query.
+    """
+    contexts = retrieve_from_database(input_query, 3)
+    prompt = generate_prompt(input_query, contexts)
+    print(prompt)
+    answer = chat_bot.get_answer_from_llm(prompt)
+    return answer
